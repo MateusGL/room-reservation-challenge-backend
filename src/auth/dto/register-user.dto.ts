bc619ea9422466cc81ea5d62ab8cdfcd347e7243
Mateus Grunheidt Lacerda <mateus.lacerda@tangerino.com.br>
2025-03-28 21:36:09 -0300

    feat: add swagger api doc

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,16 +1,20 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterUserDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
+  @ApiProperty()
   password: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
@@ -14,6 +15,16 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Reservation Room')
+    .setDescription('API endpoints [auth,reservations,rooms,users]')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {});
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3001);
 }
